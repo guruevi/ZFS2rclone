620ed37448d497d7e6f428a01ff847cbbd411fcb
guruevi <vanooste@rcbi.rochester.edu>
2018-05-10 00:05:53 -0400

    Allow for simultaneous, more robust upload

--- backup_script.sh
+++ backup_script.sh
@@ -4,6 +4,7 @@ CURRENTSNAP=$2
 SCRIPTPATH=/mnt/Backup
 DESTPATH=/mnt/Backup/tapes
 MBUFFERPATH=/mnt/Backup/jails_2/build/usr/local/bin/mbuffer
+
 # TODO: Perhaps we should save/query this on the remote?
 LASTSNAP=`cat ${DESTPATH}/${NAME}/lastsnap`
 TAPESIZE="12G" #Set this to the smaller number of what your system can handle and your destination allows
@@ -11,18 +12,29 @@ TAPESIZE="12G" #Set this to the smaller number of what your system can handle an
 #TODO: If currentsnap is not set, create one, or pick the last one if it isn't LASTSNAP
 
 # Find out if we ran this before
-
 if [[ ! -z $LASTSNAP ]]; then
   INCREMENT="-i $LASTSNAP"
 else
   INCREMENT=""
 fi
 
-#TODO: IF Remote $SNAPSHOT already exist, most likely that's a previous failure. Move or delete that remote snapshot and rewrite it from scratch?
+#TODO: IF Remote $SNAPSHOT already exist, most likely that's a previous failure. Move or delete that remote snapshot and rewrite it from scratch
 
-SEND_CMD="$SCRIPTPATH/send_script.sh $NAME $CURRENTSNAP"
+#TODO: Every year, 
 
-mkdir -p $DESTPATH/$NAME
-zfs send $INCREMENT $NAME@$CURRENTSNAP | \
-    $MBUFFERPATH -o $DESTPATH/$NAME/tapedev -D $TAPESIZE -A "$SEND_CMD" && \
-    $SEND_CMD && echo $LASTSNAP > $DESTPATH/$NAME/lastsnap && rm $DESTPATH/$NAME/tapedev
+function run_cmd() {
+    exec $1
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+    return $rc
+}
+
+MOVE_CMD="$SCRIPTPATH/move_script.sh $NAME $CURRENTSNAP"
+
+mkdir -p $DESTPATH/$NAME/$SNAPSHOT
+run_cmd zfs list -r $NAME > $DESTPATH/$NAME/volume_list
+run_cmd zfs list -r -t snapshot $NAME > $DESTPATH/$NAME/snapshot_list
+run_cmd zfs send $INCREMENT $NAME@$CURRENTSNAP | $MBUFFERPATH -o $DESTPATH/$NAME/tapedev -D $TAPESIZE -A "$MOVE_CMD"
+run_cmd $MOVE_CMD
+run_cmd $LASTSNAP > $DESTPATH/$NAME/lastsnap
+run_cmd rclone copy $DESTPATH/$NAME/lastsnap URBox:/ZFS-SEND/$NAME/
+run_cmd rm $DESTPATH/$NAME/tapedev
