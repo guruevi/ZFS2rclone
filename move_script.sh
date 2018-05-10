baa3bba18c35c3286e8bf7b793f5b3445574e74d
guruevi <vanooste@rcbi.rochester.edu>
2018-05-10 00:07:10 -0400

    Simultaneous, more robust upload

---
+++ move_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NOW=`perl -MTime::HiRes=gettimeofday -MPOSIX=strftime -e '($s,$us) = gettimeofday(); printf "%d.%06d\n", $s, $us'`
+NAME=$1
+SNAPSHOT=$2
+
+DEBUG="-vvvv --stats=30s"
+DESTPATH="/mnt/Backup/tapes"
+
+while [ `ls $DESTPATH/$NAME/$SNAPSHOT | wc -l` -gt 16 ]; do
+    sleep 5
+    echo "Waiting for free space"
+done
+
+mv $DESTPATH/$NAME/tapedev $DESTPATH/$NAME/$SNAPSHOT/$NOW
+touch $DESTPATH/$NAME/tapedev
--- send_script.sh
+++ send_script.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-NOW=`perl -MTime::HiRes=gettimeofday -MPOSIX=strftime -e '($s,$us) = gettimeofday(); printf "%d.%06d\n", $s, $us'`
-NAME=$1
-SNAPSHOT=$2
-
 DEBUG="-vvvv --stats=30s"
 DESTPATH="/mnt/Backup/tapes"
 
-rclone $DEBUG --transfers=8 --buffer-size=64M copyto $DESTPATH/$NAME/tapedev URBox:/ZFS-SEND/$NAME/$SNAPSHOT/$NOW && truncate -s 0 $DESTPATH/$NAME/tapedev
+rclone $DEBUG --transfers=16 --exclude=tapedev --exclude=lastsnap --buffer-size=64M move $DESTPATH URBox:/ZFS-SEND/
